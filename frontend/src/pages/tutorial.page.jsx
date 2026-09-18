d087916a347f57aa3412e091ac5856086baee1b9
Jeremy Nelemans <jeremy.nelemans@carcollect.com>
2026-09-18 10:09:43 +0200

    tutorial

--- frontend/src/pages/tutorial.page.jsx
+++ frontend/src/pages/tutorial.page.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 function TutorialPage() {
+  const name = "John";
+
   return (
     <div>
-      <h1>Welcome to the workshop!</h1>
+      <h1>Welcome to the workshop, {name}!</h1>
       <div>This is what we will be editing first!</div>
     </div>
   );
