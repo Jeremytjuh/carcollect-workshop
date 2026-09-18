1ca126087e7ad19612a46b6aa0ad7a31d1e9388c
Pascal <pascal.van.de.keere@carcollect.com>
2026-09-18 12:52:23 +0200

    default tuto

--- frontend/src/pages/tutorial.page.jsx
+++ frontend/src/pages/tutorial.page.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useState } from "react";
+
 function TutorialPage() {
   const name = "John";
+
+  const [textColor, setTextColor] = useState("black");
+
+  useEffect(() => {
+    console.log("The text color was changed to:", textColor);
+  }, [textColor]);
 
   return (
     <div>
       <h1>Welcome to the workshop, {name}!</h1>
-      <div>This is what we will be editing first!</div>
+      <div style={{ color: textColor }}>This is what we will be editing first!</div>
     </div>
   );
 }
