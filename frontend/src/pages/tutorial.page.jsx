5917b7615004fb0fd6067a02e10d08f27439f964
Jeremy Nelemans <jeremy.nelemans@carcollect.com>
2026-09-18 09:46:20 +0200

    tutorial page

---
+++ frontend/src/pages/tutorial.page.jsx
@@ -0,0 +1,10 @@
+function TutorialPage() {
+  return (
+    <div>
+      <h1>Welcome to the workshop!</h1>
+      <div>This is what we will be editing first!</div>
+    </div>
+  );
+}
+
+export default TutorialPage;
